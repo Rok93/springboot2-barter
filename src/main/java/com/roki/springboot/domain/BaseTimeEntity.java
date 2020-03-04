b79929688e9a6480769b55fbbf10ef9feb79dd71
김경록 <goodboy3421@gmail.com>
2020-03-04 11:53:33 +0900

    feat: Jpa Auditing 활성화, 생성시간,수정시간 기능추
    생성시간, 수정시간 (중복요소) 생성 기능

---
+++ src/main/java/com/roki/springboot/domain/BaseTimeEntity.java
@@ -0,0 +1,23 @@
+package com.roki.springboot.domain;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass // 해당 클래스를 상속할 경우 필드들도 칼럼으로 인식
+@EntityListeners(AuditingEntityListener.class) // 해당 클래스에 Auditing 기능을 포함
+public class BaseTimeEntity {
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+
+}
