84917b6445b669767287d7ac8f14b39ff1ecbd34
김경록 <goodboy3421@gmail.com>
2020-03-10 01:57:38 +0900

    fix: Repository 못 읽어들이는 버그 수정

--- src/main/java/com/roki/springboot/domain/boards/BarterBoard.java
+++ src/main/java/com/roki/springboot/domain/boards/BarterBoard.java
@@ -3,7 +3,6 @@ package com.roki.springboot.domain.boards;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.ColumnDefault;
 
 import javax.persistence.*;
 
