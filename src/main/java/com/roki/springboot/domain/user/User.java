a7601e309ade88e5cf6adc61b2eacdc66486d14e
김경록 <goodboy3421@gmail.com>
2020-03-09 11:22:28 +0900

    feat: User(회원) Entity 추가
    User Entity 추가, 컬럼 타입 (Role Enum), Repository 추가

---
+++ src/main/java/com/roki/springboot/domain/user/User.java
@@ -0,0 +1,46 @@
+package com.roki.springboot.domain.user;
+
+import com.roki.springboot.domain.boards.Board;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+//    프로필 사진 여건이 되면 추가해보기
+//    @Column
+//    private String picture;
+
+    @Builder
+    public User(String name, String email, Role role) {
+        this.name = name;
+        this.email = email;
+        this.role = role;
+    }
+
+    public String getRoleKey() {
+        return this.role.getKey();
+    }
+
+}
---
+++ src/main/java/com/roki/springboot/domain/user/UserRepository.java
@@ -0,0 +1,6 @@
+package com.roki.springboot.domain.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
